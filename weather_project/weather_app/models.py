74e5a35166bbbc435eef0460b160f8dcfb823989
David Abellán <davidabellangarrido@gmail.com>
2025-10-25 19:05:35 +0200

    [Feature] Added meteo_api_handler.py, model_handler.py and some models into models.py

--- weather_project/weather_app/models.py
+++ weather_project/weather_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    locality = models.CharField(max_length=100)
+    lat = models.FloatField()
+    long = models.FloatField()
+    elevation = models.FloatField(null=True, blank=True)  # opcional
+
+    def __str__(self):
+        return self.locality
+
+
+class HourlyWeatherData(models.Model):
+    temperature = models.FloatField()
+    precipitation = models.FloatField(null=True, blank=True)  # si luego quieres añadir precipitación
+    pressure = models.FloatField()  # surface_pressure
+    date = models.DateTimeField()
+    location: Location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="hourly_data")
+
+    def __str__(self):
+        return f"{self.date} - {self.location.locality}"
+
--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+import json
 
-# Create your views here.
+class Controller(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        pass
+
