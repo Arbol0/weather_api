cc3f1e6c64892ab947252ad17339f608d8276ba2
David Abellán <davidabellangarrido@gmail.com>
2025-10-27 14:23:08 +0100

    [Refactor] README.md

--- weather_app/tests.py
+++ weather_app/tests.py
@@ -1,14 +1,6 @@
 from django.test import TestCase
 
 """
-Ideas de tests:
-
-# test services/meteo_api_handler/get_coordinate: que pasa si escribo mal el nombre, muy mal o bien
-# coord = meteo.get_coordinates('madrid')
-
-Test solo se inserta un registro de cada
-
-Test ciudad mal escrita al insertar
 
 """
 
